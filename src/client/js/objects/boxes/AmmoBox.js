27fd42414669530d2fcb0769cb72d9be3c070e1f
ohmed <bogdan@nwgstudios.com>
2016-09-10 14:42:15 +0300

    Added improvements for tank visualization and updated resources files.

--- src/client/js/objects/boxes/AmmoBox.js
+++ src/client/js/objects/boxes/AmmoBox.js
@@ -10,6 +10,8 @@ DT.Box.Ammo = function ( params ) {
     this.type = 'AmmoBox';
     this.amount = 20;
 
+    this.animTime = 600 * Math.random() * Math.PI * 2;
+
     //
 
     this.init();
@@ -20,10 +22,10 @@ DT.Box.Ammo.prototype = Object.create( DT.Box.prototype );
 
 DT.Box.Ammo.prototype.init = function () {
 
-    var geometry = new THREE.BoxGeometry( 10, 10, 10 );
-    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    var boxModel = resourceManager.getModel( 'ammo_box.json' );
 
-    this.mesh = new THREE.Mesh( geometry, material );
+    this.mesh = new THREE.Mesh( boxModel.geometry, new THREE.MeshFaceMaterial( boxModel.material ) );
+    this.mesh.scale.set( 20, 20, 20 );
     this.mesh.position.copy( this.position );
 
     view.scene.add( this.mesh );
@@ -33,5 +35,13 @@ DT.Box.Ammo.prototype.init = function () {
 DT.Box.Ammo.prototype.remove = function () {
 
     view.scene.remove( this.mesh );
+
+};
+
+DT.Box.Ammo.prototype.update = function ( delta ) {
+
+    this.animTime += delta;
+    this.mesh.rotation.y = Math.sin( this.animTime / 600 );
+    this.mesh.position.y = Math.sin( this.animTime / 300 ) + 20;
 
 };
--- src/client/js/objects/tanks/USA-T54.js
+++ src/client/js/objects/tanks/USA-T54.js
@@ -25,6 +25,77 @@ DT.Tank.USAT54.prototype.range = 100;
 DT.Tank.USAT54.prototype.armour = 180;
 DT.Tank.USAT54.prototype.bullet = 105;
 
+DT.Tank.USAT54.prototype.initModel = function () {
+
+    this.object = new THREE.Object3D();
+
+    var tankBaseModel = resourceManager.getModel( this.model.base );
+    var tankTopModel = resourceManager.getModel( this.model.top );
+
+    //
+
+    var base = new THREE.Mesh( tankBaseModel.geometry, new THREE.MeshFaceMaterial( tankBaseModel.material ) );
+    base.castShadow = true;
+    base.rotation.y =  0;
+    base.receiveShadow = true;
+    base.scale.set( 20, 20, 20 );
+    this.object.add( base );
+    this.object.base = base;
+
+    //
+
+    var top = new THREE.Mesh( tankTopModel.geometry, new THREE.MeshFaceMaterial( tankTopModel.material ) );
+    top.castShadow = true;
+    top.receiveShadow = true;
+    top.position.y = 20;
+    top.scale.set( 20, 20, 20 );
+
+    for ( var i = 0, il = top.material.materials.length; i < il; i ++ ) {
+
+        top.material.materials[ i ].morphTargets = true;
+
+    }
+
+    this.object.add( top );
+
+    //
+
+    var box = new THREE.Mesh( new THREE.BoxGeometry( 30, 40, 60 ), new THREE.MeshLambertMaterial({ transparent: true, opacity: 0 }) );
+    box.position.y = 10;
+    box.name = 'tank';
+    box.owner = this.player;
+    box.material.visible = false;
+    this.object.add( box );
+    view.scene.intersections.push( box );
+    this.object.top = top;
+
+    //
+
+    this.mixer = new THREE.AnimationMixer( top );
+
+    var morphTargets = top.geometry.morphTargets;
+
+    var clip = THREE.AnimationClip.CreateFromMorphTargetSequence( 'shot', [ morphTargets[0], morphTargets[1], morphTargets[2] ], 30 );
+    clip.duration = 30;
+    var shotAction = this.mixer.clipAction( clip );
+    shotAction.loop = THREE.LoopOnce;
+    shotAction.enabled = false;
+    this.animations.shotAction = shotAction;
+
+    var clip = THREE.AnimationClip.CreateFromMorphTargetSequence( 'death', [ morphTargets[2], morphTargets[3], morphTargets[4] ], 10 );
+    clip.duration = 10;
+    var deathAction = this.mixer.clipAction( clip );
+    deathAction.duration = clip.duration;
+    deathAction.loop = THREE.LoopOnce;
+    deathAction.enabled = false;
+    this.animations.deathAction = shotAction;
+
+    //
+
+    view.scene.add( this.object );
+
+};
+
 //
 
 DT.Tank.list[ 'USAT54' ] = {
