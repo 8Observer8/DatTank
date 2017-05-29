f3152eaeb7ce7f9342da5db906c9dc2be600fb4f
DESKTOP-VH1PQ5H\User <vmvvmv99@gmail.com>
2017-05-29 12:33:35 +0300

    Set rendering skyBox always ( added second camera ), correct fog.

--- src/client/js/managers/ViewManager.js
+++ src/client/js/managers/ViewManager.js
@@ -28,6 +28,11 @@ Game.ViewManager = function () {
 
     this.scene = false;
     this.camera = false;
+    
+    this.skyboxScene = false;
+    this.skyBoxCamera = false;
+
+
     this.renderer = false;
 
     this.selectionCircle = false;
@@ -53,18 +58,29 @@ Game.ViewManager.prototype.setupScene = function () {
 
     this.scene = new THREE.Scene();
     this.scene.intersections = [];
-    this.camera = new THREE.PerspectiveCamera( 60, this.SCREEN_WIDTH / this.SCREEN_HEIGHT, 1, 10000 );
+    this.camera = new THREE.PerspectiveCamera( 60, this.SCREEN_WIDTH / this.SCREEN_HEIGHT, 1, 1000 );
 
+    this.skyboxScene = new THREE.Scene();
+    this.skyboxScene.intersections = [];
+    this.skyBoxCamera = new THREE.PerspectiveCamera( 60, this.SCREEN_WIDTH / this.SCREEN_HEIGHT, 1, 5000 );
 
-    this.scene.add( Game.Skybox() );
+    this.skybox = Game.Skybox();
 
+    this.skyboxScene.add( this.skybox );
 
     this.camera.position.set( 180, 400, 0 );
     this.camera.lookAt( new THREE.Vector3() );
 
+
+
+    this.skyBoxCamera.position.set( 180, 400, 0 );
+    this.skyBoxCamera.lookAt( new THREE.Vector3() );
+    this.skyboxScene.add( this.skyBoxCamera );
+
     this.scene.add( this.camera );
 
-    this.scene.fog = new THREE.Fog( 0xa9a6a6, 350, 900 );
+    this.scene.fog = new THREE.Fog( 0xa9a6a6, 200, 800 );
+    this.skyboxScene.fog = new THREE.Fog( 0xa9a6a6, 700, 4000 );
 
     // setup sound listener
 
@@ -569,10 +585,17 @@ Game.ViewManager.prototype.animate = function ( delta ) {
     this.camera.position.z = Game.arena.me.position.z - ( 120 * Math.cos( Game.arena.me.rotation ) );
     this.camera.position.y = 100;
 
-
     var lookPos = new THREE.Vector3( Game.arena.me.position.x, Game.arena.me.position.y + 65, Game.arena.me.position.z );
 
     this.camera.lookAt( lookPos );
+
+    this.skyBoxCamera.position.x = Game.arena.me.position.x - ( 120 * Math.sin( Game.arena.me.rotation ) );
+    this.skyBoxCamera.position.z = Game.arena.me.position.z - ( 120 * Math.cos( Game.arena.me.rotation ) );
+    this.skyBoxCamera.position.y = 100;
+
+
+    this.skyBoxCamera.lookAt( lookPos );
+
 
 
     if ( Game.arena.boxManager ) {
@@ -676,7 +699,13 @@ Game.ViewManager.prototype.render = function () {
 
     this.animate( delta );
 
+
+    this.renderer.render( this.skyboxScene, this.skyBoxCamera );
+    this.renderer.autoClear = false;
     this.renderer.render( this.scene, this.camera );
+    
+    
+
 
     //
 
--- src/client/js/objects/core/Skybox.js
+++ src/client/js/objects/core/Skybox.js
@@ -1,8 +1,16 @@
 Game.Skybox = function() {
 
-    var material = new THREE.MeshBasicMaterial( {  map: THREE.ImageUtils.loadTexture('resources/img/sky2.jpg'), side: THREE.BackSide } );
+    var material = new THREE.MeshBasicMaterial( {  map: THREE.ImageUtils.loadTexture('resources/img/sky_photo6.jpg'), side: THREE.BackSide, fog: true } );
 
-    var skyboxMesh = new THREE.Mesh( new THREE.SphereGeometry( 2500, 60, 40 ), material );
+    var geometry =  new THREE.SphereGeometry( 2500, 60, 40 );
+
+    geometry.computeBoundingSphere();
+
+    geometry.frustumCulled = false;
+
+    var skyboxMesh = new THREE.Mesh( geometry, material );
+
+    skyboxMesh.frustumCulled = false;
 
     return skyboxMesh;
 
