473ceedd03c71ffbd64821ef1b461a0468525a38
DESKTOP-VH1PQ5H\User <vmvvmv99@gmail.com>
2017-06-01 13:21:17 +0300

    Corrected wall collison check.

--- src/server/managers/CollisionManager.js
+++ src/server/managers/CollisionManager.js
@@ -90,6 +90,18 @@ CollisionManager.prototype.moveTank = function ( direction, player, delta ) {
 
     if ( Math.abs( newPosition.x ) > 1270 || Math.abs( newPosition.z ) > 1270 ) {
 
+        if (  player.moveDirection.x > 0 ) {
+
+            player.position.x -= ( player.moveSpeed  * Math.sin( player.rotation ) * delta);
+            player.position.z -= ( player.moveSpeed  * Math.cos( player.rotation ) * delta);
+        
+        } else if ( player.moveDirection.x < 0) {
+                    
+            player.position.x += ( player.moveSpeed   * Math.sin( player.rotation ) * delta);
+            player.position.z += ( player.moveSpeed   * Math.cos( player.rotation ) * delta);
+            
+        }
+
         return false;
 
     }
